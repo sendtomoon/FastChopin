a0073b7e10b192ad2ff35e5e5ac79425b4439fe0
galaxysha512 <galaxysha512@gmail.com>
2019-03-28 10:14:29 +0800

    jsoup解析xml

--- src/main/java/com/sendtomoon/chopin/ip/GetIP.java
+++ src/main/java/com/sendtomoon/chopin/ip/GetIP.java
@@ -3,9 +3,25 @@ package com.sendtomoon.chopin.ip;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.http.client.CookieStore;
+import org.apache.http.impl.client.BasicCookieStore;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import com.sendtomoon.chopin.entity.dto.HttpResponseDTO;
+import com.sendtomoon.chopin.tools.HttpUtils;
+
 public class GetIP {
 
 	private String ipAddr;
+
+	private final String ROUTER_LOGIN_URL = "http://192.168.0.1/login.cgi";
+
+	private final String ROUTER_STATUS_URL = "http://192.168.0.1/ajax_status.xml?hash=0.24395379430691233";
+
+	private final String ROUTER_REQ_PARAM = "group_id=&action_mode=&action_script=&action_wait=5&current_page=Main_Login.asp&next_page=index.asp&login_authorization=bGJ0NDI1OjU5MTMyMTU=";
 
 	public String ip() {
 		this.mainService();
@@ -13,7 +29,17 @@ public class GetIP {
 	}
 
 	private void mainService() {
-
+		CookieStore cookie = new BasicCookieStore();
+		HttpResponseDTO resultLogin = null;
+		HttpResponseDTO resultStatus = null;
+		try {
+			resultLogin = HttpUtils.post(ROUTER_LOGIN_URL, ROUTER_REQ_PARAM, null, this.getHeader(), cookie);
+			cookie = resultLogin.getCookie();
+			resultStatus = HttpUtils.post(ROUTER_STATUS_URL, null, null, this.getHeader(), cookie);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		Document doc = 
 	}
 
 	private Map<String, String> getHeader() {
