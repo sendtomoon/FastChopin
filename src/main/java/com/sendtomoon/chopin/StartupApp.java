6e334bd85401a36b54b2b8c2a98ebb88471d98c4
galaxysha512 <galaxysha512@gmail.com>
2019-03-28 16:57:37 +0800

    调整

--- src/main/java/com/sendtomoon/chopin/StartupApp.java
+++ src/main/java/com/sendtomoon/chopin/StartupApp.java
@@ -1,26 +1,17 @@
 package com.sendtomoon.chopin;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Component;
 
 import com.sendtomoon.chopin.service.MainService;
 
+@Component
 public class StartupApp {
 
-	private static ApplicationContext ac = null;
-
-	static {
-		ac = new ClassPathXmlApplicationContext("root-config.xml");
-	}
-
-	final static Logger logger = LoggerFactory.getLogger(StartupApp.class);
-
 	public static void main(String[] args) {
-		logger.info("Starting----------------");
+		ApplicationContext ac = new ClassPathXmlApplicationContext("root-config.xml");
 		MainService main = ac.getBean(MainService.class);
 		main.mainService();
-		logger.info("End---------------------");
 	}
 }
--- src/main/java/com/sendtomoon/chopin/service/MainService.java
+++ src/main/java/com/sendtomoon/chopin/service/MainService.java
@@ -1,9 +1,14 @@
 package com.sendtomoon.chopin.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.sendtomoon.chopin.ip.GetIP;
+
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.core.util.StatusPrinter;
 
 @Component
 public class MainService {
@@ -14,8 +19,14 @@ public class MainService {
 	@Autowired
 	private ReNewDNS renew;
 
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
 	public void mainService() {
+		LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
+        StatusPrinter.print(lc);
+		logger.info("Starting----------------");
 		renew.renew(getip.ip());
+		logger.info("End---------------------");
 	}
 
 }
