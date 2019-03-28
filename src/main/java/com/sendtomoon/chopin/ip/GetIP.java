9d620bf986c5ef593050b40755f5d43032db515f
galaxysha512 <galaxysha512@gmail.com>
2019-03-28 10:00:08 +0800

    getHeader

--- src/main/java/com/sendtomoon/chopin/ip/GetIP.java
+++ src/main/java/com/sendtomoon/chopin/ip/GetIP.java
@@ -1,15 +1,34 @@
 package com.sendtomoon.chopin.ip;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class GetIP {
-	
+
 	private String ipAddr;
 
 	public String ip() {
+		this.mainService();
 		return ipAddr;
 	}
 
 	private void mainService() {
 
+	}
+
+	private Map<String, String> getHeader() {
+		Map<String, String> header = new HashMap<String, String>();
+		header.put("Accept",
+				"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3");
+		header.put("Accept-Encoding", "gzip, deflate");
+		header.put("Accept-Language", "zh-CN,zh;q=0.9,en;q=0.8");
+		header.put("Cache-Control", "max-age=0");
+		header.put("Content-Type", "application/x-www-form-urlencoded");
+		header.put("Host", "192.168.0.1");
+		header.put("Origin", "http://192.168.0.1");
+		header.put("Proxy-Connection", "keep-alive");
+		header.put("Referer", "http://192.168.0.1/Main_Login.asp");
+		return header;
 	}
 
 }
--- src/test/java/com/sendtomoon/chopin/tests/HttpUtilsTests.java
+++ src/test/java/com/sendtomoon/chopin/tests/HttpUtilsTests.java
@@ -26,7 +26,7 @@ public class HttpUtilsTests {
 					"group_id=&action_mode=&action_script=&action_wait=5&current_page=Main_Login.asp&next_page=index.asp&login_authorization=bGJ0NDI1OjU5MTMyMTU=",
 					null, getH1(), cookie);
 			System.err.println(map.get("response"));
-			map = HttpUtils.post("http://192.168.0.1/ajax_status.xml?hash=0.24395379430691233", "", null, getH2(),
+			map = HttpUtils.post("http://192.168.0.1/ajax_status.xml?hash=0.24395379430691233", "", null, getH1(),
 					cookie);
 			System.err.println(map.get("response"));
 		} catch (Exception e) {
@@ -43,36 +43,17 @@ public class HttpUtilsTests {
 		header.put("Accept-Language", "zh-CN,zh;q=0.9,en;q=0.8");
 		header.put("Cache-Control", "max-age=0");
 		header.put("Content-Type", "application/x-www-form-urlencoded");
-		header.put("Cookie",
-				"traffic_warning_NaN=2018.7:1; apps_last=; hwaddr=0C:9D:92:49:B7:70; awrtm_wlrefresh=0; dhcp_sortcol=1; dhcp_sortmet=1; clickedItem_tab=0");
-		header.put("DNT", "1");
+//		header.put("Cookie",
+//				"traffic_warning_NaN=2018.7:1; apps_last=; hwaddr=0C:9D:92:49:B7:70; awrtm_wlrefresh=0; dhcp_sortcol=1; dhcp_sortmet=1; clickedItem_tab=0");
+//		header.put("DNT", "1");
 		header.put("Host", "192.168.0.1");
 		header.put("Origin", "http://192.168.0.1");
 		header.put("Proxy-Connection", "keep-alive");
 		header.put("Referer", "http://192.168.0.1/Main_Login.asp");
-		header.put("Upgrade-Insecure-Requests", "1");
-		header.put("User-Agent",
-				"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36");
+//		header.put("Upgrade-Insecure-Requests", "1");
+//		header.put("User-Agent",
+//				"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36");
 		return header;
 	}
 
-	private Map<String, String> getH2() {
-		Map<String, String> header = new HashMap<String, String>();
-		header.put("Accept",
-				"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3");
-		header.put("Accept-Encoding", "gzip, deflate");
-		header.put("Accept-Language", "zh-CN,zh;q=0.9,en;q=0.8");
-		header.put("Content-Type", "application/x-www-form-urlencoded");
-		header.put("Cookie",
-				"traffic_warning_NaN=2018.7:1; apps_last=; hwaddr=0C:9D:92:49:B7:70; awrtm_wlrefresh=0; dhcp_sortcol=1; dhcp_sortmet=1; clickedItem_tab=0");
-		header.put("DNT", "1");
-		header.put("Host", "192.168.0.1");
-		header.put("Origin", "http://192.168.0.1");
-		header.put("Proxy-Connection", "keep-alive");
-		header.put("Referer", "http://192.168.0.1/login.cgi");
-		header.put("Upgrade-Insecure-Requests", "1");
-		header.put("User-Agent",
-				"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36");
-		return header;
-	}
 }
