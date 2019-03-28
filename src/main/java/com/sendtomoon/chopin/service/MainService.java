0968445635e592b320e95291ad9e0e9bf430a47d
galaxysha512 <galaxysha512@gmail.com>
2019-03-28 14:27:17 +0800

    设置spring bean 注入

--- src/main/java/com/sendtomoon/chopin/service/MainService.java
+++ src/main/java/com/sendtomoon/chopin/service/MainService.java
@@ -1,8 +1,17 @@
 package com.sendtomoon.chopin.service;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import com.sendtomoon.chopin.ip.GetIP;
 
 public class MainService {
+
+	private static ApplicationContext ac = null;
+
+	static {
+		ac = new ClassPathXmlApplicationContext("root-config.xml");
+	}
 
 	public void mainService() {
 		GetIP ip = new GetIP();
