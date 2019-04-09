54a1415c6fb0dbcfa1a2a2fb0d55ae78d8ece4b1
galaxysha512 <galaxysha512@gmail.com>
2019-04-09 15:02:50 +0800

    fix

--- src/main/java/com/sendtomoon/chopin/StartupApp.java
+++ src/main/java/com/sendtomoon/chopin/StartupApp.java
@@ -1,9 +1,5 @@
 package com.sendtomoon.chopin;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
-
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -12,14 +8,7 @@ import com.sendtomoon.chopin.service.MainService;
 public class StartupApp {
 
 	static {
-		Properties prop = new Properties();
-		InputStream in = StartupApp.class.getClassLoader().getResourceAsStream("system.properties");
-		try {
-			prop.load(in);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		System.setProperty("chopin.log.dir", prop.getProperty("chopin.log.dir"));
+		System.setProperty("chopin.log.dir", "d:/logs");
 	}
 
 	public static void main(String[] args) {
